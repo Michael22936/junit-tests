2f9abb193de4ac652dcfa21b5de8ea18ea9c07c1
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-10-08 14:49:44 -0500

    complete part-two-excercise

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -5,17 +5,28 @@ import static org.junit.Assert.*;
 
 public class StudentTest {
 
-   private Student michael;
-   private Student matt;
+    private Student michael;
+    private Student matt;
+    private Cohort ceres;
+    private Student student;
+    private Student student1;
+    private Student student2;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         michael = new Student(1, "Michael");
-        matt = new Student(2,"matt");
+        matt = new Student(2, "matt");
+        student = new Student(1, "mciahel");
+        student1 = new Student(2, "mciahel");
+        student2 = new Student(3, "mciahel");
+        ceres = new Cohort();
+        ceres.addStudent(student);
+        ceres.addStudent(student1);
+        ceres.addStudent(student2);
     }
 
     @Test
-    public void testIfUniqueUser(){
+    public void testIfUniqueUser() {
 
         Student matt = null;
 
@@ -26,9 +37,9 @@ public class StudentTest {
     }
 
     @Test
-    public void testFieldsSetGet(){
+    public void testFieldsSetGet() {
 
-        assertEquals(1,michael.getId());
+        assertEquals(1, michael.getId());
 
         assertEquals(michael.getName(), "Michael");
 
@@ -37,29 +48,45 @@ public class StudentTest {
     }
 
     @Test
-    public void testIfAddGradesWorks(){
+    public void testIfAddGradesWorks() {
 
         michael.addGrade(80);
 
         assertSame(80, michael.getGrades().get(0));
 
-        assertSame(1,michael.getGrades().size());
+        assertSame(1, michael.getGrades().size());
 
     }
 
     @Test
-    public void testIfGradeAvgIsCorrect(){
-        
+    public void testIfGradeAvgIsCorrect() {
+
         michael.addGrade(80);
-        
+
         michael.addGrade(90);
 
-        assertEquals(85,michael.getAverageGrade(), 0);
+        assertEquals(85, michael.getAverageGrade(), 0);
 
         matt.addGrade(100);
 
         assertEquals(100, matt.getAverageGrade(), 0);
 
+    }
+
+    @Test
+    public void testIfAddStudentWorks() {
+        ceres.addStudent(student);
+        assertEquals(1, ceres.getStudents().size());
+        ceres.addStudent(student1);
+        assertEquals(2, ceres.getStudents().size());
+    }
+
+    @Test
+    public void testIfCohortAvgIsCorrect() {
+        student.addGrade(50);
+        student1.addGrade(50);
+        student2.addGrade(50);
+        assertEquals(50, ceres.getCohortAverage(), 0);
     }
 
 }
